aca3f792393efaf02e52d72b8d396d46c624b44a
nvonmo <NICOLE.VONMOHR@ATRAMAT.COM>
2025-04-07 10:24:33 -0600

    Update card.js

--- card.js
+++ card.js
@@ -1,6 +1,7 @@
 export function Card({ children, className }) {
   return <div className={`bg-white shadow-md rounded-2xl p-4 ${className}`}>{children}</div>;
 }
+
 export function CardContent({ children }) {
   return <div>{children}</div>;
 }
